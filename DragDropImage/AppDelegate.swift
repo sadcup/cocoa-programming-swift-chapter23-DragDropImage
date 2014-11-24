2a87d97b094c1f751a0e97cbef7ba0b2d8c74c13
Netiger <netiger@NetigerdeMacBook-Pro.local>
2014-11-24 22:08:36 +0900

    Initial Commit

---
+++ DragDropImage/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  DragDropImage
+//
+//  Created by Netiger on 14-11-24.
+//  Copyright (c) 2014年 Sadcup. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
